7bc21b1de3f5b7349d2f3fe4c20594b017b997d0
€uph0r1a <tdtri1712@gmail.com>
2025-09-27 16:43:13 +0700

    a

---
+++ html css js aptech/day 10/scriptform.js
@@ -0,0 +1,8 @@
+const add = document.getElementById("add");
+const show = document.getElementById("show");
+const change = document.getElementById("change");
+const del = document.getElementById("del");
+
+add.addEventListener("click", () => {
+    
+})
